b9ba1952fdb0566495ab7bbae5a0a3c79bf59a16
SaharakBanana <mju5804101398@gmaejo.mju.ac.th>
2018-04-20 14:47:17 +0700

    test5

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
+// interface Beer {
+//     name: String
+//     abv: number
+//     description: string
+// }
+var Beer = /** @class */ (function () {
+    function Beer(beer) {
+        this.name = beer.name;
+        this.abv = beer.abv;
+        this.description = beer.description;
+    }
+    Beer.prototype.print = function () {
+        return this.name + " (" + this.abv + "%) - " + this.description;
+    };
+    return Beer;
+}());
 function getBeers() {
-
-	return fetch("https://api.punkapi.com/v2/beers").then((resp) => {
-		return resp.json()
-	})//.then((beers) => {
-	
-	// 	beers.forEach((beer) => {
-	// 		addBeer(beer)
-	// 	})
-	// })
-
+    return fetch("https://api.punkapi.com/v2/beers").then(function (resp) {
+        return resp.json();
+    });
 }
-function addBeer(beer){
-	const $beers = $('#beers')
-	$beers.append
-			("<li>"+ beer.name + "</li>")
-
-
+function addBeer(beer) {
+    var $beers = $('#beers');
+    $beers.append("<li>" + beer.print() + "</li>");
 }
-
-function displayBeers(beers){
-	beers.forEach((beer) => {
-		addBeer(beer)
-	})
+function displayBeers(beers) {
+    beers.forEach(function (beer) {
+        addBeer(new Beer(beer));
+    });
 }
-
-getBeers().then((beers) => {
-	displayBeers((beers))
-})
-
+getBeers().then(function (beers) {
+    displayBeers((beers));
+});
 // addBeer();
 // // getBears()
