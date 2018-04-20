4f9732bd96b7f391613a5e763a4758d814f67e63
SaharakBanana <mju5804101398@gmaejo.mju.ac.th>
2018-04-20 11:43:21 +0700

    v.1

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function getBears() {
-	
+
+	fetch("https://api.punkapi.com/v2/beers").then((resp) => {
+		return resp.json()
+	}).then((bears) => {
+		console.log(bears)
+		bears.forEach(bear) => {
+			addBear(bear)
+		}
+	});
 }
+getBears()
